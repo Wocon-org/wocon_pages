7cab17340dbed5b9ab6962e58981543e795efa75
CeaserZhao <chkevin.zhao@gmail.com>
2026-02-07 12:01:25 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import { defineConfig as defineViteConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, configDefaults } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   test: {
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, 'e2e/**'],
