248621e5bc00904f0e88e8af96314873ce61afc9
CeaserZhao <chkevin.zhao@gmail.com>
2026-02-16 14:54:07 +0800

    Update DiscoverPanel.vue

--- src/components/panels/DiscoverPanel.vue
+++ src/components/panels/DiscoverPanel.vue
@@ -32,8 +32,6 @@ interface PlaceInfo {
 const currentPlace = ref<PlaceInfo | null>(null)
 const isDiscovering = ref(false)
 const isLoadingCities = ref(false)
-const showDiscoverMessage = ref(false)
-const discoverMessage = ref('')
 
 // 从Supabase获取热门城市
 const fetchPopularCities = async (): Promise<PlaceInfo[]> => {
@@ -127,17 +125,7 @@ const discoverPlace = async () => {
       </div>
     </div>
 
-    <!-- 发现消息提示 -->
-    <Transition name="message-fade">
-      <div v-if="showDiscoverMessage" class="discover-message">
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-          <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/>
-          <polyline points="3.27 6.96 12 12.01 20.73 6.96"/>
-          <line x1="12" y1="22.08" x2="12" y2="12"/>
-        </svg>
-        <span>{{ discoverMessage }}</span>
-      </div>
-    </Transition>
+
   </div>
 </template>
 
